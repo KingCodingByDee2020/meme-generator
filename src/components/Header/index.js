d1339fc275872af937289ffa523e35a6cc24daee
Manav Misra <manavm@visionwebsoft.com>
2021-02-26 16:48:56 -0600

    :lipstick: Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 const Header = () => (
-  <header>
-    <h1>Meme Generator</h1>
+  <header className="bg-gradient-to-l from-blue-800 via-indigo-400 to-violet-800 py-6">
+    <h1 className="text-center text-7xl">Meme Generator</h1>
   </header>
 );
 
