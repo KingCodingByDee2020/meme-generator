c01f5fabd13f6945ffede90d4972c695ce92ca02
Manav Misra <manavm@visionwebsoft.com>
2021-03-01 17:14:29 -0600

    :sparkles: Render Form with Input and label prop

---
+++ src/components/Main/Form/Input.js
@@ -0,0 +1,17 @@
+import { toKebabCase } from "lib";
+import PropTypes from "prop-types";
+
+const Input = ({ label }) => (
+  <div>
+    <label htmlFor={toKebabCase(label)} className="sr-only">
+      {label}
+    </label>
+    <input type="text" placeholder={label} id={toKebabCase(label)} />
+  </div>
+);
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+};
+
+export default Input;
